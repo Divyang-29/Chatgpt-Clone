d410f4ecfb8c1ee2b72683100db01debb9473b49
Divyang Chunara <divyangchunara@192.168.1.3>
2025-12-01 23:07:34 +0530

    added dotenv module

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js"
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
